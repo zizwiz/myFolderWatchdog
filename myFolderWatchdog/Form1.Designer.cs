0f9c5a14963ad09979c6f512e017bea7c7c2bb86
ziz wiz <zizwiz@engineer.com>
2026-02-01 19:12:09 +0000

    trying out closing and restarting an app

--- myFolderWatchdog/Form1.Designer.cs
+++ myFolderWatchdog/Form1.Designer.cs
@@ -29,14 +29,40 @@ namespace myFolderWatchdog
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btn_close = new System.Windows.Forms.Button();
+            this.btn_restart_app = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btn_close
+            // 
+            this.btn_close.Location = new System.Drawing.Point(593, 323);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.Size = new System.Drawing.Size(139, 42);
+            this.btn_close.TabIndex = 0;
+            this.btn_close.Text = "Close";
+            this.btn_close.UseVisualStyleBackColor = true;
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            // 
+            // btn_restart_app
+            // 
+            this.btn_restart_app.Location = new System.Drawing.Point(54, 359);
+            this.btn_restart_app.Name = "btn_restart_app";
+            this.btn_restart_app.Size = new System.Drawing.Size(139, 42);
+            this.btn_restart_app.TabIndex = 1;
+            this.btn_restart_app.Text = "Restart App";
+            this.btn_restart_app.UseVisualStyleBackColor = true;
+            this.btn_restart_app.Click += new System.EventHandler(this.btn_restart_app_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_restart_app);
+            this.Controls.Add(this.btn_close);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "myFolderWatchdog";
@@ -46,6 +72,10 @@ namespace myFolderWatchdog
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Button btn_restart_app;
     }
 }
 
--- myFolderWatchdog/Form1.cs
+++ myFolderWatchdog/Form1.cs
@@ -1,18 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace myFolderWatchdog
 {
     public partial class Form1 : Form
     {
+        private readonly string appProcessName = "YourAppName"; // without .exe
+        private readonly string appExePath = @"C:\Path\To\YourApp.exe";
+        private readonly string baseFolder = @"C:\BaseFolder";
+        private readonly TimeSpan maxAllowedGap = TimeSpan.FromMinutes(5);
+
         public Form1()
         {
             InitializeComponent();
@@ -21,6 +23,75 @@ namespace myFolderWatchdog
         private void Form1_Load(object sender, EventArgs e)
         {
             Text += " : v" + Assembly.GetExecutingAssembly().GetName().Version; // put in the version number
+
+            //timer1.Interval = 30000; // check every 30 seconds
+            //timer1.Tick += Timer1_Tick;
+            //timer1.Start();
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            try
+            {
+                string todayFolder = Path.Combine(
+                    baseFolder,
+                    DateTime.Now.Year.ToString(),
+                    DateTime.Now.Month.ToString("00"),
+                    DateTime.Now.Day.ToString("00")
+                );
+
+                if (!Directory.Exists(todayFolder))
+                    return;
+
+                var newestFile = Directory.GetFiles(todayFolder)
+                    .Select(f => new FileInfo(f))
+                    .OrderByDescending(f => f.LastWriteTime)
+                    .FirstOrDefault();
+
+                if (newestFile == null)
+                    return;
+
+                TimeSpan age = DateTime.Now - newestFile.LastWriteTime;
+
+                if (age > maxAllowedGap)
+                {
+                    RestartApp();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        private void RestartApp()
+        {
+            string processPath = "";
+            
+
+            foreach (var p in Process.GetProcessesByName("myRTSPStreamer"))
+            {
+                processPath = p.MainModule.FileName; //get full path so we can restart it
+                try { p.Kill(); }
+                catch { }
+            }
+
+            // Restart it
+            Process.Start(processPath);
+
+
+            
+        }
+
+        private void btn_close_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btn_restart_app_Click(object sender, EventArgs e)
+        {
+            RestartApp();
         }
     }
 }
