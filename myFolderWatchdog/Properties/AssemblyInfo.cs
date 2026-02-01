3e9472bf2c43401cde21a86445e972c6be99060e
zizwiz <zizwiz@engineer.com>
2026-02-01 17:59:30 +0000

    Initial upload of files
    Initial upload of files

---
+++ myFolderWatchdog/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("myFolderWatchdog")]
+[assembly: AssemblyDescription("App to check if an App is locked up")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Intelligent Engineers")]
+[assembly: AssemblyProduct("myFolderWatchdog")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5d629778-d596-4f99-a6a6-b2cf717cf919")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.*")]
+//[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
